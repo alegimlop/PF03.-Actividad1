62323f0038e0d1b73cdd96e83ae4b19cc5a494be
alegimlop <alegimlop@alu.edu.gva.es>
2025-11-05 19:21:26 +0100

    solucion errores2

---
+++ src/fibonacci.js
@@ -0,0 +1,11 @@
+const fibonacci = n => {
+  if (n < 0) throw new RangeError("solo números enteros >= 0");
+  let [a, b] = [0, 1];
+  while (n--) [a, b] = [b, a + b];
+  return a;
+  
+};
+
+export{
+  fibonacci
+}
