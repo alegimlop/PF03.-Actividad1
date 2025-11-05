8915ed80316d23c36a1b189238c03e0d56dc6654
CristianGomezLopez99 <crigomlop3@alu.edu.gva.es>
2025-11-05 17:10:17 +0100

    correcion de la funcion fibonacci

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,14 +1,22 @@
 function fibonacci(n) {
-  
-
-  let a = 0, b = 1, temp;
-
-  for (let i = 2; i <= n; i++) {
-    temp = a + b;
-    a = b;
-    b = temp;
+  if (n < 0) {
+    throw new RangeError("solo números enteros ≥ 0")
   }
 
-  return b;
+  if (n === 0) return 0
+  if (n === 1) return 1
+
+  let a = 0, b = 1, temp
+
+  for (let i = 2; i <= n; i++) {
+    temp = a + b
+    a = b
+    b = temp
+  }
+
+  return b
 }
 
+export{
+  fibonacci
+}
